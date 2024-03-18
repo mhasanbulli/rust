395fc3bd9c7e9592f98c249a0bb3ef2bf98ec19f
Mustafa <mustafa@hasanbul.li>
2024-03-18 08:45:48 +0000

    Variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is {x}");
-    x = 6;
-    println!("The value of x is {x}");
+    let x = 5;
+    
+    let x = x + 5;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x in the outer scope is: {x}");
 }
